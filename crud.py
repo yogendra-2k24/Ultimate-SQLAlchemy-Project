047a6b94e5a621648548769acbdfd5010c0c4423
Yogendra K Meshram <125588349+yogendra-2k24@users.noreply.github.com>
2026-07-03 23:03:16 +0530

    Implement book issue workflow with buisness validation

--- crud.py
+++ crud.py
@@ -1,5 +1,8 @@
 from database import SessionLocal
-from models import Book
+from models import Book, Member, IssuedBook
+
+
+# For viewing the books
 
 def view_books():
 
@@ -15,6 +18,8 @@ def view_books():
 
         session.close()
 
+
+# For Adding the books
 
 def add_book(title, author, category, price, pages, edition, available_copies):
 
@@ -42,6 +47,10 @@ def add_book(title, author, category, price, pages, edition, available_copies):
     finally:
 
         session.close()
+
+
+
+# For Updating the price of the book
 
 
 def update_book_price(title, new_price):
@@ -77,6 +86,8 @@ def update_book_price(title, new_price):
         session.close()
 
 
+# For deleting the book
+
 def delete_book(title):
 
     session = SessionLocal()
@@ -84,7 +95,7 @@ def delete_book(title):
     try:
 
         book = session.query(Book).filter(
-            book.title == title
+            Book.title == title
         ).first()
 
         if book:
@@ -96,6 +107,61 @@ def delete_book(title):
             return True
     
         return False
+
+    except Exception:
+
+        session.rollback()
+
+        raise
+
+    finally:
+
+        session.close()
+
+
+def issue_book(book_id, member_id):
+
+    session = SessionLocal()
+
+    try:
+
+        book = session.query(Book).filter(
+            Book.book_id == book_id
+        ).first()
+
+        if not book:
+            return "Book Not Found"
+        
+        if book.available_copies == 0:
+            return "Book Not Available"
+        
+
+        member = session.query(Member).filter(
+            Member.member_id == member_id
+        ).first()
+
+        if not member:
+            return "Member not Found!"
+        
+        total_books = session.query(IssuedBook).filter(
+            IssuedBook.member_id == member_id,
+            IssuedBook.status == 'Issued'
+        ).count()
+
+        if total_books >= 3:
+            return "Maximum Issue Limit Reached"
+        
+        new_issue = IssuedBook(
+            book_id = book.book_id,
+            member_id = member.member_id,
+            status = 'Issued'
+        )
+
+        session.add(new_issue)
+
+        book.available_copies -= 1
+
+        session.commit()
 
     except Exception:
 
