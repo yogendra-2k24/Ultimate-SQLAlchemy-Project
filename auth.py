849a33ee4a6ea4d8e95ca9ae83b4d83a690e0323
Yogendra K Meshram <125588349+yogendra-2k24@users.noreply.github.com>
2026-07-22 02:01:23 +0530

    Implement JWT access token verification

--- auth.py
+++ auth.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+from jose import jwt
+import crud
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -10,3 +12,20 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def verify_access_token(token: str):
+
+    payload = jwt.decode(
+        token,
+        crud.SECRET_KEY,
+        algorithms=[crud.ALGORITHM]
+
+    )
+
+    user_id = payload.get("sub")
+
+    if user_id is None:
+        return None
+    
+    return user_id
